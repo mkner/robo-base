eedf6d16599d53aa4a3c922c48c85c8c8cd2cf0c
Mike Knerr <53237403+mkner@users.noreply.github.com>
2024-01-16 16:44:37 -0500

    Update __init__.py

--- robobase/__init__.py
+++ robobase/__init__.py
@@ -4,14 +4,14 @@ robo-base - primary objects
 
 # flit uses __version__ as one source version 
 
-__version__ = 'v0.01.05a'
+__version__ = 'v0.01.05b'
 
 # import acts to export 
 
 from .object import Object
 from .system import System
 
-#
+
 
 
 
