3bf45b80e82943cd798d0cfeab6498ded96386e5
Mike Knerr <53237403+mkner@users.noreply.github.com>
2024-01-16 16:50:01 -0500

    Update __init__.py

--- robobase/__init__.py
+++ robobase/__init__.py
@@ -1,8 +1,7 @@
-"""
-robo-base - primary objects 
-"""
 
-# flit uses __version__ as one source version 
+# robo-base - primary objects 
+
+# flit et al uses __version__ as one source version 
 
 __version__ = 'v0.01.05b'
 
